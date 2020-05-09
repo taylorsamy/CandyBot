46a0804f94da03ab17a938332d923c5039cd03d9
Taylor Amy <taylorsamy@users.noreply.github.com>
2020-05-09 13:40:27 -0600

    fixed special rate
    Signed-off-by: Taylor Amy <taylorsamy@users.noreply.github.com>

--- src/main/java/org/candyShop/eventListeners/MessageReceivedListener.java
+++ src/main/java/org/candyShop/eventListeners/MessageReceivedListener.java
@@ -3,6 +3,7 @@ package org.candyShop.eventListeners;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.candyShop.Main;
 import org.candyShop.helpers.TreasureHuntUtils;
 
 import javax.annotation.Nonnull;
@@ -14,21 +15,28 @@ public class MessageReceivedListener extends ListenerAdapter {
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
 
 
-        if (!event.getAuthor().isBot()) {
-            Random rand = new Random(System.currentTimeMillis());
-            int chance = rand.nextInt(TreasureHuntUtils.frequency) + 1;
+            if (!event.getAuthor().isBot()) {
+                Random rand = new Random(System.currentTimeMillis());
+                int chance = rand.nextInt(TreasureHuntUtils.frequency) + 1;
 
-            if (chance == 1) {
-                String messageID = event.getMessageId();
-                ReactionListener listener = new ReactionListener(messageID);
-                if (rand.nextInt(TreasureHuntUtils.specialFreq) == 1) {
+                if (chance == 1) {
+                    String messageID = event.getMessageId();
+                    ReactionListener listener = new ReactionListener(messageID);
+                    if (rand.nextInt(TreasureHuntUtils.specialFreq + 1) == 1) {
 
-                    if (TreasureHuntUtils.emote[1] != null) {
+                        if (TreasureHuntUtils.emote[1] != null) {
 
-                        String newMessageID = event.getChannel().sendMessage(TreasureHuntUtils.emote[1].getAsMention()).complete().getId();
-                        event.getTextChannel().retrieveMessageById(newMessageID).complete().addReaction(TreasureHuntUtils.emote[1]).complete();
-                        listener = new ReactionListener(newMessageID);
+                            String newMessageID = event.getChannel().sendMessage(TreasureHuntUtils.emote[1].getAsMention()).complete().getId();
+                            event.getTextChannel().retrieveMessageById(newMessageID).complete().addReaction(TreasureHuntUtils.emote[1]).complete();
+                            listener = new ReactionListener(newMessageID);
 
+                        } else {
+                            if (TreasureHuntUtils.emote[0] != null) {
+                                event.getMessage().addReaction(TreasureHuntUtils.emote[0]).complete();
+                            } else {
+                                event.getMessage().addReaction(TreasureHuntUtils.emoji[0]).complete();
+                            }
+                        }
                     } else {
                         if (TreasureHuntUtils.emote[0] != null) {
                             event.getMessage().addReaction(TreasureHuntUtils.emote[0]).complete();
@@ -36,18 +44,11 @@ public class MessageReceivedListener extends ListenerAdapter {
                             event.getMessage().addReaction(TreasureHuntUtils.emoji[0]).complete();
                         }
                     }
-                } else {
-                    if (TreasureHuntUtils.emote[0] != null) {
-                        event.getMessage().addReaction(TreasureHuntUtils.emote[0]).complete();
-                    } else {
-                        event.getMessage().addReaction(TreasureHuntUtils.emoji[0]).complete();
-                    }
+                    event.getJDA().addEventListener(listener);
                 }
-                event.getJDA().addEventListener(listener);
             }
-        }
-    }
 
+    }
 
     public void terminate(JDA jda) {
         jda.removeEventListener(this);
