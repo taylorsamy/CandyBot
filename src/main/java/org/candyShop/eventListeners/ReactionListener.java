3f0d398d7cee4aa9b76352b184ce395716d486e2
Taylor Amy <taylorsamy@users.noreply.github.com>
2020-06-15 00:07:32 -0600

    removed try again system for treasure hunt
    Signed-off-by: Taylor Amy <taylorsamy@users.noreply.github.com>

--- src/main/java/org/candyShop/eventListeners/ReactionListener.java
+++ src/main/java/org/candyShop/eventListeners/ReactionListener.java
@@ -12,29 +12,29 @@ import java.util.Random;
 
 public class ReactionListener extends ListenerAdapter {
     private String messageID;
-    private int find = 0;
-    private int chance = new Random().nextInt(2);
+//    private int find = 0;
+//    private int chance = new Random().nextInt(2);
 
-    LinkedList<Integer> messageQueue = new LinkedList<>();
+//    LinkedList<Integer> messageQueue = new LinkedList<>();
 
 
     public ReactionListener(String messageID) {
         this.messageID = messageID;
-        initQueue();
+//        initQueue();
 
 
     }
 
-    private void initQueue() {
-        messageQueue.add(0);
-        messageQueue.add(1);
-        messageQueue.add(2);
-        messageQueue.add(3);
-        messageQueue.add(4);
-        messageQueue.add(5);
-        messageQueue.add(6);
-        Collections.shuffle(messageQueue);
-    }
+//    private void initQueue() {
+//        messageQueue.add(0);
+//        messageQueue.add(1);
+//        messageQueue.add(2);
+//        messageQueue.add(3);
+//        messageQueue.add(4);
+//        messageQueue.add(5);
+//        messageQueue.add(6);
+//        Collections.shuffle(messageQueue);
+//    }
 
     @Override
     public void onMessageReactionAdd(@Nonnull MessageReactionAddEvent event) {
@@ -48,7 +48,7 @@ public class ReactionListener extends ListenerAdapter {
 //                if (event.getMember().getIdLong() != TreasureHuntUtils.lastCollected.getIdLong()) {
 
 
-                    if (chance == find) {
+//                    if (chance == find) {
 //                        TreasureHuntUtils.lastCollected = event.getMember();
                         if (event.getTextChannel().retrieveMessageById(messageID).complete().getAuthor().isBot()) {
                             if (TreasureHuntUtils.emote[2] != null) {
@@ -68,10 +68,10 @@ public class ReactionListener extends ListenerAdapter {
                         event.getChannel().retrieveMessageById(messageID).complete().clearReactions().complete();
                         TreasureHuntUtils.addRoles(event.getGuild(), event.getUserId());
                         event.getJDA().removeEventListener(this);
-                    } else {
-                        sendTryAgainMessage(event);
-                        find++;
-                    }
+//                    } else {
+//                        sendTryAgainMessage(event);
+//                        find++;
+//                    }
 
 
 //                } else { //member collected the last one
@@ -80,33 +80,33 @@ public class ReactionListener extends ListenerAdapter {
             }
         }
     }
-
-    private void sendTryAgainMessage(@Nonnull MessageReactionAddEvent event) {
-        if (messageQueue.size() == 0) {
-            initQueue();
-        }
-        int rand = messageQueue.pop();
-
-
-        if (rand == 0) {
-            event.getChannel().sendMessage("The " + TreasureHuntUtils.treasureName + " slips out of " + event.getMember().getEffectiveName() + "'s tired arms").queue();
-
-        } else if (rand == 1) {
-            event.getChannel().sendMessage("There is no room in " + event.getMember().getEffectiveName() + "'s backpack for the " + TreasureHuntUtils.treasureName).queue();
-
-        } else if (rand == 2) {
-            event.getChannel().sendMessage("The " + TreasureHuntUtils.treasureName + " doesnt budge!").queue();
-
-        } else if (rand == 3) {
-            event.getChannel().sendMessage("It was a fake" + TreasureHuntUtils.treasureName + "! Back to searching!").queue();
-
-        } else if (rand == 4) {
-            event.getChannel().sendMessage("The sun is in " + event.getMember().getEffectiveName() + "'s eyes and someone else grabs the " + TreasureHuntUtils.treasureName + " before they can see it!").queue();
-        } else if (rand == 5) {
-            event.getChannel().sendMessage(event.getMember().getEffectiveName() + " has a sugar high and is too hyper to get the " + TreasureHuntUtils.treasureName + "!").queue();
-        } else if (rand == 6) {
-            event.getChannel().sendMessage(event.getMember().getEffectiveName() + " doesn't like that " + TreasureHuntUtils.treasureName + " so they put it back!").queue();
-        }
-    }
+//
+//    private void sendTryAgainMessage(@Nonnull MessageReactionAddEvent event) {
+//        if (messageQueue.size() == 0) {
+//            initQueue();
+//        }
+//        int rand = messageQueue.pop();
+//
+//
+//        if (rand == 0) {
+//            event.getChannel().sendMessage("The " + TreasureHuntUtils.treasureName + " slips out of " + event.getMember().getEffectiveName() + "'s tired arms").queue();
+//
+//        } else if (rand == 1) {
+//            event.getChannel().sendMessage("There is no room in " + event.getMember().getEffectiveName() + "'s backpack for the " + TreasureHuntUtils.treasureName).queue();
+//
+//        } else if (rand == 2) {
+//            event.getChannel().sendMessage("The " + TreasureHuntUtils.treasureName + " doesnt budge!").queue();
+//
+//        } else if (rand == 3) {
+//            event.getChannel().sendMessage("It was a fake" + TreasureHuntUtils.treasureName + "! Back to searching!").queue();
+//
+//        } else if (rand == 4) {
+//            event.getChannel().sendMessage("The sun is in " + event.getMember().getEffectiveName() + "'s eyes and someone else grabs the " + TreasureHuntUtils.treasureName + " before they can see it!").queue();
+//        } else if (rand == 5) {
+//            event.getChannel().sendMessage(event.getMember().getEffectiveName() + " has a sugar high and is too hyper to get the " + TreasureHuntUtils.treasureName + "!").queue();
+//        } else if (rand == 6) {
+//            event.getChannel().sendMessage(event.getMember().getEffectiveName() + " doesn't like that " + TreasureHuntUtils.treasureName + " so they put it back!").queue();
+//        }
+//    }
 
 }
